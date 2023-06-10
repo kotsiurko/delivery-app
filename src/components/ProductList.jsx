405cbd5679c8a0d61ee317a704199088e8c07019
Roman <kotsiurko@gmail.com>
2023-06-10 18:18:30 +0300

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,12 +1,9 @@
 import { Grid } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import ProductItem from "./ProductItem";
-import { useLocation } from "react-router-dom";
 
 // function ProductList({ store1Arr }) {
 function ProductList({ pathname }) {
-  console.log("pathname :>> ", pathname);
-
   let productList = [];
   const { fakestoreArr } = useSelector((state) => state.fakestore);
   const { escuelajsArr } = useSelector((state) => state.escuelajs);
@@ -16,9 +13,6 @@ function ProductList({ pathname }) {
   if (pathname === "/escuelajs") {
     productList = escuelajsArr;
   }
-
-  console.log("productList :>> ", productList);
-  // const dispatch = useDispatch();
 
   return (
     <Grid
